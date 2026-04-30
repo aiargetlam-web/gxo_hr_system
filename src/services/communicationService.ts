feb06489afcc5163b847ee418f6bf0ed13a8ac2c
aiargetlam-web <aiargetlam@gmail.com>
2026-04-30 14:19:23 +0200

    Delete src directory

--- src/services/communicationService.ts
+++
@@ -1,9 +0,0 @@
-import api from './api';
-import { Communication } from '../types';
-
-export const communicationService = {
-  getCommunications: async (): Promise<Communication[]> => {
-    const response = await api.get<Communication[]>('/communications/');
-    return response.data;
-  }
-};
--- src/services/ticketService.ts
+++
@@ -1,9 +0,0 @@
-import api from './api';
-import { Ticket } from '../types';
-
-export const ticketService = {
-  getTickets: async (): Promise<Ticket[]> => {
-    const response = await api.get<Ticket[]>('/tickets/');
-    return response.data;
-  }
-};

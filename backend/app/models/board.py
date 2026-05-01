d551463ff42cfb4fc3cff837be2d0f82abe851be
aiargetlam-web <aiargetlam@gmail.com>
2026-05-01 19:04:13 +0200

    Add files via upload

--- backend/app/models/board.py
+++ backend/app/models/board.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.db.base_class import Base

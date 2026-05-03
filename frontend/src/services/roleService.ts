ce11abad2bb203c110fffdd972dc5ae170a276ef
aiargetlam-web <aiargetlam@gmail.com>
2026-05-03 21:01:53 +0200

    Add files via upload

--- frontend/src/services/roleService.ts
+++ frontend/src/services/roleService.ts
@@ -3,7 +3,7 @@ import { Role } from "../types";
 
 export const roleService = {
   async getRoles(): Promise<Role[]> {
-    const res = await api.get("/roles");
+    const res = await api.get("/api/v1/roles");
     return res.data;
   }
 };

6835925d42be0c92c359d44b79da2d08b7440c6a
aiargetlam-web <aiargetlam@gmail.com>
2026-06-22 21:03:33 +0200

    Add files via upload

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,8 +1,19 @@
-from app.db.base_class import Base
-from app.models.user import User
-from app.models.communication import CommunicationType, Communication, CommunicationAttachment
-from app.models.ticket import TicketType, Ticket, TicketMessage
-from app.models.site import Site, HRSite
-from app.models.board import BoardFile, BoardFileSite
+from app.models.employee import Employee
+from app.models.employee_contracts import EmployeeContract
+from app.models.employee_benefits import EmployeeBenefit
+from app.models.employee_site_history import EmployeeSiteHistory
+from app.models.employee_status_history import EmployeeStatusHistory
+from app.models.employee_enac_approvals import EmployeeEnacApproval
+from app.models.employee_enac_courses import EmployeeEnacCourse
+from app.models.employee_company_cars import EmployeeCompanyCar
+from app.models.employee_salaries import EmployeeSalary
+from app.models.employee_departments import EmployeeDepartment
+from app.models.employee_cost_centers import EmployeeCostCenter
+
+from app.models.role import Role
+from app.models.site import Site
+from app.models.board import Board
+from app.models.ticket import Ticket
+from app.models.communication import Communication
+from app.models.audit import Audit
 from app.models.import_users_log import ImportUsersLog
-from app.models.audit import ActivityLog, UserHistoryLog

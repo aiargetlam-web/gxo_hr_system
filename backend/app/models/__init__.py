cb3272c07cf20baf79889ee568fc8ccb62bd3aca
aiargetlam-web <aiargetlam@gmail.com>
2026-06-28 14:50:56 +0200

    Add files via upload

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,3 @@
-# ⚠️ Importiamo SOLO i modelli principali.
-# Gli altri vengono caricati automaticamente da Base.metadata.
-
 from app.models.employee import Employee
 from app.models.role import Role
 from app.models.site import Site

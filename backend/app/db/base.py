c7d0df4b18a31ce798a9a6a0697c6b444ef23c66
aiargetlam-web <aiargetlam@gmail.com>
2026-06-28 15:25:43 +0200

    Add files via upload

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,27 +1,4 @@
 from app.db.base_class import Base
 
-from app.models.role import Role
-from app.models.site import Site
-
-from app.models.employee import Employee
-from app.models.employee_contracts import EmployeeContract
-from app.models.employee_benefits import EmployeeBenefit
-from app.models.employee_site_history import EmployeeSiteHistory
-from app.models.employee_status_history import EmployeeStatusHistory
-from app.models.employee_enac_approvals import EmployeeEnacApproval
-from app.models.employee_enac_courses import EmployeeEnacCourse
-from app.models.employee_company_cars import EmployeeCompanyCar
-from app.models.employee_salaries import EmployeeSalary
-from app.models.employee_departments import EmployeeDepartment
-from app.models.employee_cost_centers import EmployeeCostCenter
-
-from app.models.board import BoardFile, BoardFileSite
-from app.models.ticket import Ticket
-from app.models.communication import Communication
-from app.models.audit import ActivityLog, UserHistoryLog
-from app.models.import_users_log import ImportUsersLog
-from app.models.employment_status_type import EmploymentStatusType
-from app.models.department import Department
-from app.models.cost_center import CostCenter
-from app.models.work_regime import WorkRegime
-from app.models.contract_nature import ContractNature
+# Importiamo SOLO i modelli principali
+from app.models import Employee, Role, Site

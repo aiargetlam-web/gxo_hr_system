053865c1d3f9e3a738af9f6cb3dc4a9ffb95afaf
aiargetlam-web <aiargetlam@gmail.com>
2026-07-12 18:57:47 +0200

    Add files via upload

--- backend/app/api/api_v1/endpoints/sites.py
+++ backend/app/api/api_v1/endpoints/sites.py
@@ -3,11 +3,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.api import deps
-from app.schemas.sites import SiteDict  # <-- CAMBIATO QUI
+from app.schemas.site import SiteDict   # <-- QUI CAMBIATO
 
 router = APIRouter()
 
-@router.get("/", response_model=List[SiteDict])  # <-- CAMBIATO QUI
+@router.get("/", response_model=List[SiteDict])   # <-- QUI CAMBIATO
 def get_sites(
     db: Session = Depends(deps.get_db),
     current_user = Depends(deps.get_current_user)

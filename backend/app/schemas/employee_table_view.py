94c0127afdeff2521e7250fc758729795d25f38e
aiargetlam-web <aiargetlam@gmail.com>
2026-08-09 17:58:05 +0200

    Add files via upload

---
+++ backend/app/schemas/employee_table_view.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import List
+
+class EmployeeTableViewBase(BaseModel):
+    name: str
+    columns: List[str]
+
+class EmployeeTableViewCreate(EmployeeTableViewBase):
+    user_id: int
+
+class EmployeeTableViewUpdate(EmployeeTableViewBase):
+    pass
+
+class EmployeeTableView(EmployeeTableViewBase):
+    id: int
+    user_id: int
+
+    class Config:
+        orm_mode = True

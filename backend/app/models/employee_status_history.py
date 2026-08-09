92b089483167fb69d0aac1f0fe16c40d9ad343c4
aiargetlam-web <aiargetlam@gmail.com>
2026-08-09 19:08:51 +0200

    Add files via upload

--- backend/app/models/employee_status_history.py
+++ backend/app/models/employee_status_history.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Date, Text, ForeignKey
+from sqlalchemy import Column, Integer, Date, Text, ForeignKey, String
 from sqlalchemy.orm import relationship
 from app.db.base_class import Base
 

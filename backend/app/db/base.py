eb4252a097c747a957bd9720b6fa082ce92a2492
aiargetlam-web <aiargetlam@gmail.com>
2026-08-29 15:13:55 +0200

    Add files via upload

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -30,6 +30,7 @@ from app.models.ticket import Ticket
 from app.models.board import BoardFile, BoardFileSite
 from app.models.employee_table_view import EmployeeTableView
 from app.models.employee import Employee
+from app.models.employer import Employer
 from app.models.employee_employer_history import EmployeeEmployerHistory
 from app.models.employee_union_history import EmployeeUnionHistory
 from app.models.law_104_type import Law104Type
